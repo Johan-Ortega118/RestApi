a07f773e4e19c5b706315a163089f1256aa55dc1
Johan-Ortega118 <85240547+Johan-Ortega118@users.noreply.github.com>
2021-12-15 19:40:07 -0600

    Update 20211213010340-create-user.js

--- database/migrations/20211213010340-create-user.js
+++ database/migrations/20211213010340-create-user.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('HaloCharacter', {
+    await queryInterface.createTable('User', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -31,6 +31,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('HaloCharacter');
+    await queryInterface.dropTable('User');
   }
 };
